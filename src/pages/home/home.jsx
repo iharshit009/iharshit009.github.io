61baf171a9675a5571464fea3f6769f14cf40923
iharshit009 <harshitjain1309@gmail.com>
2020-03-29 22:03:14 +0530

    final for today

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,47 +1,42 @@
 import React, { Component } from "react";
 import Me from '../../static/projects/me.png'
 import Me0 from '../../static/projects/me0.png'
-import Me1 from '../../static/projects/me1.png'
-import Me2 from '../../static/projects/me2.png'
-import Me3 from '../../static/projects/me3.png'
+
 import Me4 from '../../static/projects/me4.png'
-// import Me5 from '../../static/projects/me5.png'
-// import Me6 from '../../static/projects/me6.png'
-// import Me7 from '../../static/projects/me7.png'
+
 import "./home.scss";
 
-
-// const imagesPath = {
-//   Me: Me,
-//   Me0: Me0,
-//   Me1: Me1,
-//   Me2: Me2,
-//   Me3: Me3,
-//   Me4: Me4,
-//   // Me5 : "Me5",
-//   // Me6: "Me6",
-//   // Me7 : "Me7",
-//   // Me8 : "Me8",
-//   // Me9 : "Me9",
-// }
 
 
 export default class Home extends Component {
   state = {
+    count: 0,
     imageName: Me
   }
 
 
   toggleImage = () => {
-    this.setState({ imageName: Me1 })
+    this.setState({ count: this.state.count + 1 })
+    switch (this.state.count) {
+      case 0:
+
+        this.setState({ imageName: Me0 })
+        break
+
+      case 1:
+        this.setState({ imageName: Me4 })
+
+    }
+    if (this.state.count == 1) {
+      this.setState({ count: 0 })
+    }
   }
-  // getImageName = () => this.setState({imageName : }) 
+
   render() {
     return (
       <div className="home">
-        {/* <img  src =  {Me} alt = "iharshit009" style = {{borderRadius : 9, margin : 30}}  /> */}
 
-        <img className="my_image" style={{ borderRadius: 9, margin: 60, }} src={this.state.imageName} onClick={this.toggleImage} />
+        <img className="my_image" style={{ borderRadius: 9, margin: 60, height: 330 }} src={this.state.imageName} onClick={this.toggleImage} />
         <div className="home_name">
           Hello! This is Harshit Jain
           <br />
