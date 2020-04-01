65a0f8e8ee20ed42bd54a57b8c3fe08946861582
iharshit009 <harshitjain1309@gmail.com>
2020-04-01 17:52:33 +0530

    fixed header scrolling

--- src/pages/header/header.jsx
+++ src/pages/header/header.jsx
@@ -3,17 +3,17 @@ import "./header.scss";
 import { NavLink as Link } from "react-router-dom";
 
 export default class Header extends Component {
-  // componentDidMount() {
-  //   if (window.innerWidth >= 900) {
-  //     window.onscroll = function() {
-  //       if (window.pageYOffset === 0) {
-  //         document.getElementById("heade").classList.remove("scroll");
-  //       } else {
-  //         document.getElementById("heade").classList.add("scroll");
-  //       }
-  //     };
-  //   }
-  // }
+  componentDidMount() {
+    if (window.innerWidth >= 900) {
+      window.onscroll = function () {
+        if (window.pageYOffset === 0) {
+          document.getElementById("heade").classList.remove("scroll");
+        } else {
+          document.getElementById("heade").classList.add("scroll");
+        }
+      };
+    }
+  }
   render() {
     return (
       <div id="heade" className="nav">
